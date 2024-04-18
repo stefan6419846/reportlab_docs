8d4b962b831d55c0d1b3ff97556d0e256754ded6
Stefan <96178532+stefan6419846@users.noreply.github.com>
2024-04-18 19:53:37 +0200

    add recommonmark extension for Markdown

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ version = release = metadata["Version"]
 # General configuration
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx"]
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.intersphinx", "recommonmark"]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
